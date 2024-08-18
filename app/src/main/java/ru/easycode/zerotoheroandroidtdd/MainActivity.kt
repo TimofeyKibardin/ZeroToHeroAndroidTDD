5d6ce5fddf5604d8dfa5df562118c8676fc037d9
TimofeyKibardin <timoshka.kartoshka99@gmail.com>
2024-08-18 10:16:41 +0300

    ZeroToHero/Task 6 Done by java objects

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,15 +3,39 @@ package ru.easycode.zerotoheroandroidtdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        /*Вариант с xml. В принципе, если не прям много вьюх - то можно через xml
+        setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.changeButton)
         val textView = findViewById<TextView>(R.id.titleTextView)
+
+        button.setOnClickListener {
+            textView.setText("I am an Android Developer!")
+        }
+        */
+
+        //Вариант через Java-объекты
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.id = R.id.rootLayout
+
+        val textView = TextView(this)
+        textView.id = R.id.titleTextView
+        textView.setText("Hello World!")
+        linearLayout.addView(textView)
+
+        val button = Button(this)
+        button.id = R.id.changeButton
+        button.setText("change")
+        linearLayout.addView(button)
+
+        setContentView(linearLayout)
 
         button.setOnClickListener {
             textView.setText("I am an Android Developer!")
