efba74d3f63672295a14ba0ee529f47af865d0c4
TimofeyKibardin <timoshka.kartoshka99@gmail.com>
2024-08-19 22:13:54 +0300

    ZeroToHero/Task 7 Done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,15 +6,32 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var textView = findViewById<TextView>(R.id.titleTextView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.changeButton)
-        val textView = findViewById<TextView>(R.id.titleTextView)
+        textView = findViewById(R.id.titleTextView)
 
         button.setOnClickListener {
             textView.setText("I am an Android Developer!")
         }
+    }
+
+    //Сохраняем при пересоздании активити
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", textView.text.toString())
+    }
+
+    //Восстанавливаем значения
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textView.text = savedInstanceState.getString("key")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
